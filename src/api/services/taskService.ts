32ec8219bdf460708c428dd63033c22bf9ef1834
Siarhei <siarhei.by@gmail.com>
2022-11-15 21:20:52 +0300

    feat: add tasks service

---
+++ src/api/services/taskService.ts
@@ -0,0 +1,55 @@
+import API from 'api/api';
+import { TTaskRes, TTaskResExt, TTaskParams, TTaskParamsExt, TTaskSet } from 'api/types';
+
+export function getTasksByColumn(boardId: string, columnId: string): Promise<TTaskResExt[]> {
+  return API.get(`/boards/${boardId}/columns/${columnId}/tasks`).then((res) => res.data);
+}
+
+export function createTask(
+  boardId: string,
+  columnId: string,
+  task: TTaskParams
+): Promise<TTaskRes> {
+  return API.post(`/boards/${boardId}/columns/${columnId}/tasks`, task).then((res) => res.data);
+}
+
+export function getTaskById(
+  boardId: string,
+  columnId: string,
+  taskId: string
+): Promise<TTaskResExt> {
+  return API.get(`/boards/${boardId}/columns/${columnId}/tasks/${taskId}`).then((res) => res.data);
+}
+
+export function updateTaskById(
+  boardId: string,
+  columnId: string,
+  taskId: string,
+  task: TTaskParamsExt
+): Promise<TTaskResExt> {
+  return API.put(`/boards/${boardId}/columns/${columnId}/tasks/${taskId}`, task).then(
+    (res) => res.data
+  );
+}
+
+export function deleteTaskById(
+  boardId: string,
+  columnId: string,
+  taskId: string
+): Promise<TTaskResExt> {
+  return API.delete(`/boards/${boardId}/columns/${columnId}/tasks/${taskId}`).then(
+    (res) => res.data
+  );
+}
+
+export function getTaskSet(ids: string[], userId: string, search: string): Promise<TTaskResExt[]> {
+  return API.get(`/taskSet`, { params: { ids, userId, search } }).then((res) => res.data);
+}
+
+export function updateTaskSet(tasks: TTaskSet): Promise<TTaskResExt[]> {
+  return API.patch(`/taskSet`, tasks).then((res) => res.data);
+}
+
+export function getTaskSetByBoard(boardId: string): Promise<TTaskResExt[]> {
+  return API.get(`/taskSet/${boardId}`).then((res) => res.data);
+}
--- src/api/types.ts
+++ src/api/types.ts
@@ -1,15 +1,78 @@
-export type TSignInRes = {
-  token: 'string';
+export type TSignInParams = {
+  login: string;
+  password: string;
 };
 
-export type TUserReq = {
-  name: 'string';
-  login: 'string';
-  password: 'string';
+export type TSignInRes = {
+  token: string;
+};
+
+export type TUserPrams = {
+  name: string;
+  login: string;
+  password: string;
 };
 
 export type TUserRes = {
-  _id: 'string';
-  name: 'string';
-  login: 'string';
+  _id: string;
+  name: string;
+  login: string;
+};
+
+export type TBoardParams = {
+  title: string;
+  owner: string;
+  users: string[];
+};
+
+export type TBoardRes = {
+  _id: string;
+  title: string;
+  owner: string;
+  users: string[];
+};
+
+export type TColParams = {
+  title: string;
+  order: number;
+};
+
+export type TColParamsExt = TColParams & { boardId: string };
+
+export type TListColParams = {
+  _id: string;
+  order: number;
+};
+
+export type TColRes = {
+  _id: string;
+  title: string;
+  order: number;
+  boardId: string;
+};
+
+export type TTaskParams = {
+  title: string;
+  order: number;
+  description: string;
+  userId: string;
+  users: string[];
+};
+
+export type TTaskParamsExt = TTaskParams & { columnId: string };
+
+export type TTaskRes = {
+  title: string;
+  order: number;
+  description: string;
+  userId: string;
+  users: string[];
+};
+
+export type TTaskResExt = TTaskRes & { _id: string; boardId: string; columnId: string };
+
+export type TTaskSet = {
+  _id: string;
+  order: number;
+  columnId: string;
 };
