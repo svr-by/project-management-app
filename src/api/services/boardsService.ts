28841c99a09ce8901b2b057762bbee6a790b4ddc
Siarhei <siarhei.by@gmail.com>
2022-11-15 19:45:23 +0300

    feat: add column service

--- src/api/services/boardsService.ts
+++ src/api/services/boardsService.ts
@@ -1,26 +1,26 @@
 import API from 'api/api';
-import { TBoardParams, TBoardsRes } from 'api/types';
+import { TBoardParams, TBoardRes } from 'api/types';
 
-export function getAllBoards(): Promise<TBoardsRes[]> {
+export function getAllBoards(): Promise<TBoardRes[]> {
   return API.get('/boards').then((res) => res.data);
 }
 
-export function createBoard(board: TBoardParams): Promise<TBoardsRes> {
+export function createBoard(board: TBoardParams): Promise<TBoardRes> {
   return API.post('/boards', board).then((res) => res.data);
 }
 
-export function getBoardById(id: string): Promise<TBoardsRes> {
+export function getBoardById(id: string): Promise<TBoardRes> {
   return API.get(`/boards/${id}`).then((res) => res.data);
 }
 
-export function updateBoardById(id: string, board: TBoardParams): Promise<TBoardsRes> {
+export function updateBoardById(id: string, board: TBoardParams): Promise<TBoardRes> {
   return API.put(`/boards/${id}`, board).then((res) => res.data);
 }
 
-export function getBoardsByIds(ids: string[]): Promise<TBoardsRes[]> {
-  return API.get(`/boardsSet`, { params: ids }).then((res) => res.data);
+export function getBoardsByIds(ids: string[]): Promise<TBoardRes[]> {
+  return API.get(`/boardsSet`, { params: { ids } }).then((res) => res.data);
 }
 
-export function getBoardsByUserId(userId: string[]): Promise<TBoardsRes[]> {
+export function getBoardsByUserId(userId: string[]): Promise<TBoardRes[]> {
   return API.get(`/boardsSet/${userId}`).then((res) => res.data);
 }
