91d4b701355a129b3195dcf186521473f70e53df
Ihar Levachkov <pokerlis@gmail.com>
2022-11-20 16:11:23 +0300

    feat: add a partal

---
+++ src/components/portal/Portal.tsx
@@ -0,0 +1,25 @@
+import { useEffect, useRef } from 'react';
+import ReactDOM from 'react-dom';
+
+type PortalProps = {
+  children: React.ReactNode;
+};
+
+const Portal = ({ children }: PortalProps) => {
+  const elRef = useRef<HTMLDivElement | null>(null);
+  if (!elRef.current) {
+    elRef.current = document.createElement('div');
+  }
+  const el = elRef.current;
+
+  useEffect(() => {
+    document.body.appendChild(el);
+    return () => {
+      document.body.removeChild(el);
+    };
+  }, [el]);
+
+  return ReactDOM.createPortal(children, el);
+}
+
+export { Portal };
