a5fc99ae7185fee6b2a5d7343a705ef31c328b75
Siarhei <siarhei.by@gmail.com>
2022-12-04 19:29:57 +0300

    feat: fix bug with translator

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,4 @@
+import { Suspense } from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
@@ -10,9 +11,11 @@ const container = document.getElementById('root')!;
 const root = createRoot(container);
 
 root.render(
-  <BrowserRouter>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </BrowserRouter>
+  <Suspense fallback={<div>Loading...</div>}>
+    <BrowserRouter>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </BrowserRouter>
+  </Suspense>
 );
