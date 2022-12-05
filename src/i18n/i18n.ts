80b36ad5a7cd5abfc291e258f9e1d2253ad29a08
Siarhei <siarhei.by@gmail.com>
2022-12-05 23:02:47 +0300

    feat: add adaptive for BoardPage

--- src/i18n/i18n.ts
+++ src/i18n/i18n.ts
@@ -1,5 +1,4 @@
 import i18n from 'i18next';
-// import Backend from 'i18next-http-backend';
 import LanguageDetector from 'i18next-browser-languagedetector';
 import { initReactI18next } from 'react-i18next';
 import en from './locales/en/translation.json';
@@ -11,7 +10,6 @@ const resources = {
 };
 
 i18n
-  // .use(Backend)
   .use(LanguageDetector)
   .use(initReactI18next)
   .init({
--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,7 @@
-// import { Suspense } from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import { store } from './redux/store';
-// import { Spinner } from 'components';
 import App from 'app/App';
 import './index.scss';
 import './i18n/i18n';
@@ -12,11 +10,9 @@ const container = document.getElementById('root')!;
 const root = createRoot(container);
 
 root.render(
-  // <Suspense fallback={<Spinner />}>
   <BrowserRouter>
     <Provider store={store}>
       <App />
     </Provider>
   </BrowserRouter>
-  // </Suspense>
 );
